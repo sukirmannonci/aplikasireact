dee2f17c6f18e9d9e24d705bb2dd9ede757913c1
sukirmannonci <sukirman.nonci@gmail.com>
2021-08-10 18:34:31 +0800

    Berhasil menampilkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -4,5 +4,16 @@ var response = require('./rest');
 var connection = require('./connect');
 
 exports.index = function (req,res) {
-    response.ok("Aplikasi rest api sudah berjalan")
+    response.ok("Aplikasi rest api sudah berjalan", res)
+};
+
+// Menampilkan semua data mahasiswa
+exports.tampilSemuaMahasiswa = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fields) {
+        if (error) {
+            connection.log(error);
+        }else{
+            response.ok(rows, res);
+        }
+    });
 };
--- route.js
+++ route.js
@@ -5,4 +5,7 @@ module.exports = function (app) {
 
     app.route('/')
         .get(json.index);
+
+    app.route('/tampil')
+        .get(json.tampilSemuaMahasiswa);    
 }
